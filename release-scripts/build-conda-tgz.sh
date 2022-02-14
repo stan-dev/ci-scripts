7df24adee3646c82bb69727eae22db6485738c86
Nicusor Serban <serban.nicusor@toptal.com>
2022-02-15 00:17:30 +0100

    Added conda build script

---
+++ release-scripts/build-conda-tgz.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Script used to install cmdstan on a GColab Machine and create a .tgz archive with the installation
+# Usage example:
+# On a fresh colab instance simply run:
+# !curl https://gist.githubusercontent.com/serban-nicusor-toptal/412fb920f80f85b5e217e38ca86d5cbc/raw/7946e9f315e82537276d06843e1b9186c505bcfb/conda-cmdstan.sh | bash -s -- 2.29.0
+# You can follow all the progress in the console, should take around 10 minutes
+# At the end the console will print a URL where you can download the .tgz archive
+
+VERSION="$1"
+INSTALLATION_HOME="/root"
+
+echo "Creating v$VERSION conda .tgz archive !"
+
+# Install cmdstanpy
+pip install --upgrade cmdstanpy
+
+# Install cmdstan
+python <<HEREDOC
+from cmdstanpy import install_cmdstan
+install_cmdstan(cores=2, progress=True)
+HEREDOC
+
+# Create tgz archive
+cd $INSTALLATION_HOME/.cmdstan; tar -cf - cmdstan-$VERSION | gzip > $INSTALLATION_HOME/cmdstan-$VERSION.tgz
+tar -tzf $INSTALLATION_HOME/cmdstan-$VERSION.tgz | head
+
+# Use transfer.sh to upload our achive for easy retrieval
+curl --upload-file /root/cmdstan-$VERSION.tgz https://transfer.sh/cmdstan-$VERSION.tgz
