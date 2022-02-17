d3707b0ab23e2ca93170676bd29505323933bd54
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2022-02-17 20:26:25 +0100

    tmp stuff

--- release-scripts/release-notes.py
+++ release-scripts/release-notes.py
@@ -4,10 +4,10 @@
 
 import urllib.request, json
 
-repository = "math"
-release_url = "https://api.github.com/repos/stan-dev/"+repository+"/releases/latest"
-releases = json.loads(urllib.request.urlopen(release_url).read().decode())
-last_release_date = releases["published_at"]
+repository = "docs"
+# release_url = "https://api.github.com/repos/stan-dev/"+repository+"/releases/latest"
+# releases = json.loads(urllib.request.urlopen(release_url).read().decode())
+last_release_date = "2021-10-18T00:00:00Z"
 
 prs_url = "https://api.github.com/repos/stan-dev/"+repository+"/pulls?state=closed&sort=created&per_page=100&sort=asc&page="
 
@@ -15,10 +15,15 @@ prs_url = "https://api.github.com/repos/stan-dev/"+repository+"/pulls?state=clos
 num_of_prs = 0
 prs_on_page = 1
 current_page = 1
+users = {}
+
 # cycle through the pages until you hit a page with no PRs
 while prs_on_page > 0:
     tmp_url = prs_url + str(current_page)
-    with urllib.request.urlopen(tmp_url) as url:
+    request = urllib.request.Request(tmp_url)
+    token = "githubpat"
+    request.add_header("Authorization", "Basic %s" % token)
+    with urllib.request.urlopen(request) as url:
         prs_info = json.loads(url.read().decode())
         prs_on_page = len(prs_info)
         for pr in prs_info:
@@ -28,6 +33,11 @@ while prs_on_page > 0:
                 if pr["merged_at"] > last_release_date:
                     num_of_prs = num_of_prs + 1
                     body = pr["body"]
+                    user = pr["user"]["login"]
+                    if user in users:
+                        users[user] = users[user] + 1
+                    else:
+                        users[user] = 1
                     parsing_release_notes = False
                     for line in body.split("\r\n"):
                         if parsing_release_notes:
@@ -39,6 +49,8 @@ while prs_on_page > 0:
                                 print(" - " + line + "(#" + str(pr["number"]) + ")")
                         if line.find("Release notes") >= 0:
                             parsing_release_notes = True
+    print(current_page)
     current_page = current_page + 1
 
 print("\nNumber of merged PRs:" + str(num_of_prs))
+print(users)
