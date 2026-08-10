dd915f900818a0f7fabfc782e5b9946981eb975d
Brian Ward <bward@flatironinstitute.org>
2026-08-10 14:21:59 -0400

    Update release-notes getter

--- release-scripts/release-notes.py
+++ release-scripts/release-notes.py
@@ -8,27 +8,35 @@ import json
 import argparse
 import os
 
-parser = argparse.ArgumentParser(description='Get release notes from merged PRs since last release')
+parser = argparse.ArgumentParser(
+    description="Get release notes from merged PRs since last release"
+)
 parser.add_argument("repo", help="repository name")
 parser.add_argument("--time", help="time of last release", default="fetch")
 
 args = parser.parse_args()
 repository = args.repo
 
-REPO_PR_FORMAT = {"stanc3": "Release notes",
-                  "math": "Release notes",
-                  "stan": "Summary",
-                  "cmdstan": "Summary",}
+REPO_PR_FORMAT = {
+    "stanc3": "Release notes",
+    "math": "Release notes",
+    "stan": "Summary",
+    "cmdstan": "Summary",
+}
 
 try:
     TOKEN = os.environ["GITHUB_TOKEN"]
-    header = {"Authorization":"Bearer " + TOKEN}
+    header = {"Authorization": "Bearer " + TOKEN}
 except:
-    print("No GITHUB_TOKEN environment variable found. This mail fail if you hit the rate limit.")
+    print(
+        "No GITHUB_TOKEN environment variable found. This mail fail if you hit the rate limit."
+    )
     header = {}
 
 if args.time == "fetch":
-    release_url = "https://api.github.com/repos/stan-dev/"+repository+"/releases/latest"
+    release_url = (
+        "https://api.github.com/repos/stan-dev/" + repository + "/releases/latest"
+    )
     request = urllib.request.Request(release_url, headers=header)
     releases = json.loads(urllib.request.urlopen(request).read().decode())
     last_release_date = releases["published_at"]
@@ -36,7 +44,11 @@ else:
     last_release_date = datetime.datetime.fromisoformat(args.time).isoformat()
 
 
-prs_url = "https://api.github.com/repos/stan-dev/"+repository+"/pulls?state=closed&sort=created&per_page=100&sort=asc&page="
+prs_url = (
+    "https://api.github.com/repos/stan-dev/"
+    + repository
+    + "/pulls?state=closed&sort=created&per_page=100&sort=asc&page="
+)
 
 print("Release notes for", repository, "since", last_release_date)
 
@@ -56,14 +68,20 @@ while prs_on_page > 0:
             # if merged check date
             if pr["merged_at"] > last_release_date:
                 num_of_prs = num_of_prs + 1
-                body = pr["body"]
+                body = pr["body"] or ""
                 if REPO_PR_FORMAT[repository] in body:
                     parsing_release_notes = 0
                     for line in body.split("\r\n"):
                         if parsing_release_notes:
                             if line.find("##") >= 0:
                                 if parsing_release_notes == 1:
-                                    print(" - " + pr['title'] + " (#" + str(pr['number']) +")\n")
+                                    print(
+                                        " - "
+                                        + pr["title"]
+                                        + " (#"
+                                        + str(pr["number"])
+                                        + ")\n"
+                                    )
                                 elif parsing_release_notes == 2:
                                     print(" (#" + str(pr["number"]) + ")\n")
                                 elif parsing_release_notes == 3:
@@ -72,17 +90,19 @@ while prs_on_page > 0:
 
                             line = line.strip()
                             if len(line) > 0:
-                                if parsing_release_notes == 1: # first line
-                                    parsing_release_notes = 2 # next line would be second
+                                if parsing_release_notes == 1:  # first line
+                                    parsing_release_notes = (
+                                        2  # next line would be second
+                                    )
                                     print(" - " + line, end="")
                                 else:
                                     print("\n\t" + line, end="")
-                                    parsing_release_notes = 3 # we had a second line
+                                    parsing_release_notes = 3  # we had a second line
 
                         if line.find(REPO_PR_FORMAT[repository]) >= 0:
                             parsing_release_notes = 1
                 else:
-                    print(" - " + pr['title'] + " (#" + str(pr['number']) +")\n")
+                    print(" - " + pr["title"] + " (#" + str(pr["number"]) + ")\n")
 
     current_page = current_page + 1
 
